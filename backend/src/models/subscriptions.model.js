35e2e87131dcae6933322a9fa2cc9966aa132a64
Ambi <ambi17289@gmail.com>
2025-04-27 12:12:52 +0530

    add subscription model

---
+++ backend/src/models/subscriptions.model.js
@@ -0,0 +1,14 @@
+import mongoose , { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber : {
+        type : Schema.Types.ObjectId, //One whose is subscribing
+        ref : "User" 
+    },
+    channel : {
+        type : Schema.Types.ObjectId, //One to whom "subscriber" is subscribing to
+        ref : "User"
+    }
+},{timestamps : true})
+
+export const Subscription = mongoose.Model(subscriptionSchema, "Subscription")
